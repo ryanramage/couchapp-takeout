1dd406561d421b0b3287d0d1e561b515a17e6422
Ryan Ramage <ryan.ramage@gmail.com>
2011-07-16 10:23:19 -0600

    jQuery plugin that shows a link on the remote app, and only text on the local app.

---
+++ couchapp/html/takeout.js
@@ -0,0 +1,52 @@
+/**
+ * Created by .
+ * User: ryan
+ * Date: 11-07-16
+ * Time: 9:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+(function( $ ){
+  $.fn.takeout = function(options) {
+
+    var match = /\/([a-zA-Z0-9_-]+)\//.exec(window.location);
+    var db = match[1];
+    var linkText = "Install";
+    var localText = "Running Local."
+
+
+    if (options && options.db) {
+        db = options.db;
+    }
+    if (options && options.linkText) {
+        linkText = options.linkText;
+    }
+    if (options && options.localText) {
+        localText = options.localText;
+    }
+
+    var takeoutLocalDoc = "/" + db + "/_local/takeout" ;
+    var div = this;
+    $.ajax({
+      url: takeoutLocalDoc,
+      success: function(data) {
+          div.each(function() {
+              var $this = $(this);
+              $this.text(localText);
+
+          })
+      },
+      error: function() {
+           div.each(function() {
+              var $this = $(this);
+              $this.append('<a class="takeoutLink" href="_show/takeout.jnlp">'+ linkText + '</a>');
+
+          })         
+      },
+      dataType: 'json'
+    });
+
+
+
+    return this;  
+  };
+})( jQuery );
