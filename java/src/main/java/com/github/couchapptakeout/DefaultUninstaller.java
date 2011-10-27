fced6386061abd94de471e3c2b2e843ca33334ac
Ryan Ramage <ryan.ramage@gmail.com>
2011-10-27 00:27:31 -0600

    Starting uninstaller.

---
+++ java/src/main/java/com/github/couchapptakeout/DefaultUninstaller.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.github.couchapptakeout;
+
+
+
+
+/**
+ *
+ * @author ryan
+ */
+public class DefaultUninstaller implements Uninstaller {
+
+
+    public void uninstall() {
+        
+    }
+
+
+}
